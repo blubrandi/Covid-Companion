b17214e76ebed66fe067346e5b9a94bbcd19da3a
Brandi Taylor <brandi@Brandis-MacBook-Pro.local>
2020-08-06 12:39:11 -0600

    Added Xcode Project

---
+++ Covid Companion/Covid Companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid Companion
+//
+//  Created by Brandi Taylor on 8/6/20.
+//  Copyright © 2020 Squee, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
